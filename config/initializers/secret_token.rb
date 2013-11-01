839da4698f150b5601ff36b5ed58ec8910d4d01d
Ugesen Pillay <ugesen@walmartlabs.com>
2013-11-01 13:43:08 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Financy::Application.config.secret_token = 'fba7b1a4035dc32f77258f780e76ded83b09d3ccf34e7b1247f5a19230ebc66cdd63450f831f10cac7f5464b72a96fcc692fff9d59ba1870570b147950ace762'
