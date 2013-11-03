8432d45dda651befb70675664b500128b4f2bfad
Ugesen Pillay <ugesen@walmartlabs.com>
2013-11-03 00:32:14 -0700

    added bootstrap + homepage

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Financy::Application.initialize!
+
+APP_CONFIG = YAML::load(File.open("#{Rails.root}/config/config.yml"))
