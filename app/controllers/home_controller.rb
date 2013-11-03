e6f813ee4f605db5151a4ddc482b19af56a5b537
Ugesen Pillay <ugesen@walmartlabs.com>
2013-11-03 00:49:54 -0700

    added basic auth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
+  before_filter :authenticate
+
   def index
+  end
+
+  protected
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+    username == APP_CONFIG['username'] && password == APP_CONFIG['password']
+    end
   end
 end
