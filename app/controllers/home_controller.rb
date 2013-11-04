852b51427607e6c19e2486f57cd695926a07f639
Ugesen Pillay <ugesen@walmartlabs.com>
2013-11-04 10:58:48 -0800

    temp removed auth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-http_basic_authenticate_with :name => APP_CONFIG['username'], :password => APP_CONFIG['password']
+#http_basic_authenticate_with :name => APP_CONFIG['username'], :password => APP_CONFIG['password']
 
   def index
   end
